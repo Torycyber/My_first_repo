2dc3538f724e5a1a51ae12f2b7ceb5d5b498dd65
Torycyber <victorasaph001@gmail.com>
2024-09-23 11:06:34 +0000

    modified to precis

--- .github/Trading_bot/Trading_bot.py
+++ .github/Trading_bot/Trading_bot.py
@@ -85,16 +85,16 @@ def calculate_indicators(symbol,timeframe,days,indicators,**kwargs):
 			elif indicators=='bbands':
 				bbands=ti.bbands(Data[:,4],period,stddev)
 				return bbands
-			elif indicators=='obv':
-				obv=ti.obv(Data[:,4],Data[:,5])
-				return obv
+			elif indicators=='stoch':
+				stoch=ti.stoch(Data[:,2],Data[:,3],Data[:,4],50,3,3)
+				return stoch
 
 def Tconf_Buy(High):
-			Tconf=High[-5:]
+			Tconf=High[-20:]
 			return max(Tconf)
 
 def Tconf_sell(low):
-			Tconf=low[-5:]
+			Tconf=low[-20:]
 			return min(Tconf)
 
 
@@ -112,33 +112,34 @@ def current_price(symbol):
 	ticker=exchange.fetchTicker(symbol)
 	return float(ticker['last'])
 
-def buy_sl(q,ticker):
+def buy_sl(symbol,q,ticker):
 	buy_sl=ticker-q
-	return buy_sl
+	return exchange.price_to_precision(symbol,buy_sl)
 
-def sell_sl(q,ticker):
+def sell_sl(symbol,q,ticker):
 	sell_sl=ticker+q
-	return sell_sl
+	return exchange.price_to_precision(symbol,sell_sl)
 
-def Tg_ssl(q,ticker):
+def Tg_ssl(symbol,q,ticker):
 	U=q*0.05
 	b=q-U
 	tg=ticker+b
-	return tg
-def Tg_bsl(q,ticker):
+	return exchange.price_to_precision(symbol,tg)
+def Tg_bsl(symbol,q,ticker):
 	U=q*0.05
 	b=q-U
 	tg=ticker-b
-	return tg
-def Tp_buy(q,ticker):
+	return exchange.price_to_precision(symbol,tg)
+
+def Tp_buy(symbol,q,ticker):
 	b=3*q
 	Tp=ticker+b
-	return Tp
+	return exchange.price_to_precision(symbol,Tp)
 
-def Tp_sell(q,ticker):
+def Tp_sell(symbol,q,ticker):
 	b=3*q
 	tp=ticker-b
-	return tp
+	return exchange.price_to_precision(symbol,tp)
 
 		
 
@@ -150,7 +151,7 @@ def place_order(symbol,timeframe,days):
 		low=Data[:,3]
 		close=Data[:,4]
 
-		obv=calculate_indicators(symbol,timeframe='3m',days=days,indicators='obv',period=1)
+		stoch=calculate_indicators(symbol,timeframe='3m',days=days,indicators='stoch',period=50)
 		time.sleep(1)
 		
 		ma5i=calculate_indicators(symbol,timeframe='3m',days=days,indicators='sma',period=5)
@@ -164,13 +165,13 @@ def place_order(symbol,timeframe,days):
 		bbandiii=calculate_indicators(symbol,timeframe='1h',days=days,indicator='bbands',period=10,stddev=2)
 		ma20=calculate_indicators(symbol,timeframe='3m',days=days,indicators='sma',period=20)
 		mbi=bbandi[1]
-
+		K=stoch[0]
+		d=stoch[1]
 		mbiii=bbandiii[1]
 		lbi=bbandi[0]
 		ub=bbandi[2]
 		Tconfbuy=Tconf_Buy(High)
-		Tconfibuy=Tconf_Buy(obv)
-		Tconfisell=Tconf_sell(obv)
+	
 		Tconfsell=Tconf_sell(low)
 	
 	
@@ -178,36 +179,38 @@ def place_order(symbol,timeframe,days):
 	Buy_cond2=ma5i[-1] >mbi[-1]
 	Buy_cond4=ma5iii[-1]>mbiii[-1]
 	Buy_cond5=close[-1]>Tconfbuy
-	Buy_cond3=obv[-1]>Tconfibuy
+	Buy_cond3=K[-1] > d[-1] and 30 < K[-1]< 70
 	
 		
 	Sell_cond1=ma5i[-1] <ma20[-1]
 	Sell_cond2=ma5i[-1] <mbi[-1]
-	Sell_cond3=obv[-1]<Tconfisell(obv)
+	Sell_cond3=K[-1] < d[-1]
 	Sell_cond4=ma5iii[-1]<mbiii[-1]
 	Sell_cond5=close[-1]<Tconfsell
 
 	q=ub[-1]-lbi[-1]
 	Amount=fibbs_value/q
+	Q=exchange.amount_to_precision(symbol,Amount)
 	
 	if Buy_cond1 and Buy_cond2 and Buy_cond3 and Buy_cond4 and Buy_cond5:
-		order=exchange.create_market_buy_order(symbol,Amount)
+		order=exchange.create_market_buy_order(symbol,Q)
 		ticker=current_price(symbol)
-		sl=buy_sl(q,ticker)
-		T_bsl=Tg_bsl(q,ticker)
-		Tp=Tp_buy(q,ticker)
+		sl=buy_sl(symbol,q,ticker)
+		T_bsl=Tg_bsl(symbol,q,ticker)
+		
+		Tp=Tp_buy(symbol,q,ticker)
 
-		stoploss_order=exchange.create_stop_limit_order(symbol,'sell',Amount,price=sl,stopPrice=T_bsl)
-		take_profit=exchange.create_take_profit_order(symbol,'market','sell',Amount,price=Tp)
+		stoploss_order=exchange.create_stop_limit_order(symbol,'sell',Q,price=sl,stopPrice=T_bsl)
+		take_profit=exchange.create_take_profit_order(symbol,'market','sell',Q,price=Tp)
 		return order,stoploss_order,take_profit
 	elif Sell_cond1 and Sell_cond2 and Sell_cond3 and Sell_cond4 and Sell_cond5:
-		order=exchange.create_market_sell_order(symbol,Amount)
-		sl=sell_sl(q,ticker)
-		T_sll=Tg_ssl(q,ticker)
-		Tp=Tp_sell(q,ticker)
-		stoploss_order=exchange.create_stop_limit_order(symbol,'buy',Amount,price=sl,stopPrice=T_sll)
+		order=exchange.create_market_sell_order(symbol,Q)
+		sl=sell_sl(symbol,q,ticker)
+		T_sll=Tg_ssl(symbol,q,ticker)
+		Tp=Tp_sell(symbol,q,ticker)
+		stoploss_order=exchange.create_stop_limit_order(symbol,'buy',Q,price=sl,stopPrice=T_sll)
 		time.sleep(1)
-		take_profit=exchange.create_take_profit_order(symbol,'market','buy',Amount,price=Tp)
+		take_profit=exchange.create_take_profit_order(symbol,'market','buy',Q,price=Tp)
 
 		return order,stoploss_order,take_profit
 def check_positions(symbol):
